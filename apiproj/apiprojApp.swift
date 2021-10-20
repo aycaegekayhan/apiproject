e78fc95a73959356abbc3b70f3a031f23a94657c
Ayça ege Kayhan <aycaegekayhan@Aycas-MacBook-Air.local>
2021-10-20 10:27:29 -0400

    inital commit

---
+++ apiproj/apiprojApp.swift
@@ -0,0 +1,17 @@
+//
+//  apiprojApp.swift
+//  apiproj
+//
+//  Created by Ayça ege Kayhan on 10/20/21.
+//
+
+import SwiftUI
+
+@main
+struct apiprojApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
