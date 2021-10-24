73c8b162cc19d65a0b12e11191747e353b345d94
Ayça ege Kayhan <aycaegekayhan@aycas-air.home>
2021-10-24 19:43:49 -0400

    picker view row and column selection added

--- ViewController.swift
+++ ViewController.swift
@@ -8,7 +8,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1 // pick how many columns in the picker
+    }
+    
+    let coinManager = CoinManager()
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return coinManager.currencyArray.count // pick how many rows picker should have
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return coinManager.currencyArray[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        print(row)
+    }
+    
     
     @IBOutlet weak var bitcoinLabel:UILabel!
     @IBOutlet weak var currencyLabel:UILabel!
@@ -17,7 +35,9 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        currencyPicker.dataSource = self
+        currencyPicker.delegate = self //
     }
 
 
